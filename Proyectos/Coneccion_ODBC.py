09a85d80064f6f784390521547a226822d7be876
Astorgato <crist.astorga@gmail.com>
2024-09-13 14:08:05 -0300

    Conección a SQL con pyODBC

--- Proyectos/Coneccion_ODBC.py
+++ Proyectos/Coneccion_ODBC.py
@@ -5,7 +5,7 @@ conn = mysql.connector.connect(
     host='localhost',           # O la dirección del servidor MySQL
     database='ServerPrueba', # Nombre de la base de datos
     user='root',           # Usuario de la base de datos
-    password='Gato1234!'     # Contraseña del usuario
+    password='Contraseña'     # Contraseña del usuario
 )
 if conn.is_connected():
     print("Conexión exitosa")
